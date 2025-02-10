5aa4f4ea6bdbd723d4499038b7eabac965eeff8f
andreleskinen <andre.leskinen@gmail.com>
2025-02-10 13:39:30 +0100

    update

--- obj/Debug/net8.0/AndreLeskinen1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AndreLeskinen1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AndreLeskinen1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a46cade74e72a32c484d58f577d47b3eedad437")]
 [assembly: System.Reflection.AssemblyProductAttribute("AndreLeskinen1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AndreLeskinen1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
